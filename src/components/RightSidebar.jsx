14c475ab2f33e2ad7cbf3160c0af772fc0811498
Ravi Kumar <04ravi.rk@gmail.com>
2024-11-09 01:42:54 +0530

    bug fix

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GoArrowUpRight } from "react-icons/go";
 
 const RightSidebar = () => {
   const experiences = [
@@ -32,7 +33,14 @@ const RightSidebar = () => {
       ],
       liveLink: "https://book-manager-two.vercel.app/",
       repoLink: "https://github.com/theravi04/bookManager",
-      technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "CORS", "useContext"],
+      technologies: [
+        "React.js",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "CORS",
+        "useContext",
+      ],
     },
     {
       title: "CodeChef Scraper",
@@ -42,13 +50,7 @@ const RightSidebar = () => {
       ],
       liveLink: "https://github.com/theravi04/CodeChef-Scraper",
       repoLink: "https://github.com/theravi04/CodeChef-Scraper",
-      technologies: [
-        "React.js",
-        "Node.js",
-        "Express.js",
-        "Cors",
-        "Puppeteer",
-      ],
+      technologies: ["React.js", "Node.js", "Express.js", "Cors", "Puppeteer"],
     },
     {
       title: "Appointment System",
@@ -92,20 +94,32 @@ const RightSidebar = () => {
             DSA questions on platforms like LeetCode and GeeksforGeeks using
             C++.
           </p>
-          <p className="text-lg md:text-xl font-medium">
+          <p className="text-lg md:text-xl font-medium mb-4">
             I have also participated in various contests and hackathons,
             including Smart India Hackathon (SIH) and GeeksforGeeks Job-A-Thon,
             achieving notable placements.
           </p>
+          <p className="flex text-lg md:text-xl font-medium">
+            <a
+              href="https://drive.google.com/file/d/12u88lAik5S4_NPzGMC9zgGQn3-RSAxdI/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex text-lg md:text-xl font-medium hover:text-[#0093e9] font-['Inconsolata']"
+            >
+              My Resume <GoArrowUpRight />
+            </a>
+          </p>
         </div>
 
         {/* Experience Section */}
-        <div className="experience  p-6 md:p-10 mb-10 md:mb-6">
+        <div className="experience  p-6 md:p-10 mb-10 md:mb-4">
           {/* <div className="experience  border border-gray-300 rounded-lg p-6 md:p-10 mb-10 md:mb-16 shadow-md"> */}
           <h2 className="text-3xl font-bold mb-4">Experience</h2>
           {experiences.map((exp, index) => (
             <div key={index} className="experience-item mb-6">
-              <p className="hover:text-[#0093e9] title text-2xl font-semibold">{exp.title}</p>
+              <p className="title text-2xl font-semibold">
+                <span className="hover:text-[#0093e9]">{exp.title}</span>
+              </p>
               <p className="duration mb-2">{exp.duration}</p>
               <div className="work-details">
                 {exp.work.map((item, i) => (
@@ -129,13 +143,13 @@ const RightSidebar = () => {
         </div>
 
         {/* Projects Section */}
-        <div className="projects  p-6 md:p-10 ">
+        <div className="p-6 md:p-10 ">
           {/* <div className="projects  border border-gray-300 rounded-lg p-6 md:p-10 shadow-md"> */}
           <h2 className="text-3xl font-bold mb-4">Projects</h2>
           {projects.map((project, index) => (
             <div key={index} className="project mb-6">
-              <p className="hover:text-[#0093e9] text-2xl font-semibold">
-                {project.title}
+              <p className="text-2xl font-semibold">
+                <span className="hover:text-[#0093e9]">{project.title}</span>
               </p>
               <div className="work-details">
                 {project.description.map((item, i) => (
@@ -144,22 +158,24 @@ const RightSidebar = () => {
                   </p>
                 ))}
               </div>
-              <div className="project-links mb-2 flex flex-wrap gap-2">
+              <div className=" project-links mb-2 flex flex-wrap gap-2">
                 <a
                   href={project.liveLink}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="border hover:text-[#0093e9]  px-2 py-1 rounded-md"
+                  className="flex border hover:text-[#0093e9]  px-2 py-1 rounded-md"
                 >
                   Live Link
+                    <GoArrowUpRight />
                 </a>
                 <a
                   href={project.repoLink}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="border hover:text-[#0093e9] px-2 py-1 rounded-md"
+                  className="flex border hover:text-[#0093e9] px-2 py-1 rounded-md"
                 >
                   GitHub Repo
+                  <GoArrowUpRight />
                 </a>
               </div>
               <div className="tech-used flex flex-wrap gap-2">
